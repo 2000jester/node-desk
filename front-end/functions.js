fd720f4913100e20582345f0af3776edd155ca93
Jesse Dux <jesse.dux@4mation.com.au>
2017-09-25 08:49:00 +1000

    initial commit

---
+++ front-end/functions.js
@@ -0,0 +1,162 @@
+var url = 'http://192.168.0.157:3000';
+var socket = null;
+var username = "User"
+var uuid = '';
+var desks = [];
+var staff = [];
+function sortDesks(){
+    //console.log(desks)
+    desks = _.sortBy(desks, [function(o){return parseInt(o.desk_code.substring(5),10);}])
+}
+function validate(){
+    $("#submit").removeAttr("disabled");
+    $('select').css('background', 'white');
+    $('select').css('color', 'black');
+    var data = getValuesToSubmit();
+    var errorCaught = false;
+    for(var i=0; i<data.length; i++){
+        var tempStaff = data[i]
+        for(var j=0; j<data.length; j++){
+            if((tempStaff.staff_id != data[j].staff_id)&&(tempStaff.new_desk_id == data[j].new_desk_id)){
+                errorCaught = true;
+                $("[value="+ data[j].new_desk_id +"]:selected").closest('select').css('background', 'red');
+                $("[value="+ data[j].new_desk_id +"]:selected").closest('select').css('color', 'white');
+            }
+        }
+    }
+    if(errorCaught == true){
+        $("#submit").attr("disabled", "disabled");
+        return false;
+    }
+    return true;
+}
+function display(){
+    $("tr[id^=staff_]").remove();
+    sortDesks();
+    for(var currentStaff of staff) {
+        var $element = $('#row-template').clone();
+        $element.find('.name').html(currentStaff.name);
+        $element.find('.id').html(currentStaff.id);
+        var $select = $('#desk_code_select').clone();
+        $select.attr('id',currentStaff.id);
+        for(var currentDesk of desks){
+            var $option = $('.desk_code_option').clone();
+            $option.removeClass('desk_code_option');
+            $option.html(currentDesk.desk_code);   
+            $option.appendTo($select);
+            $option.val(currentDesk.id);
+            if(currentDesk.id == currentStaff.desk_id){
+                $option.attr('selected','selected');
+            }
+        }
+        $select.appendTo($element.find(".for_select"))
+        $element.attr('id', "staff_"+currentStaff.id);
+        $element.appendTo('#desks');
+    }
+}
+function getValuesToSubmit(){
+    var valuesToSubmit = []
+    for(var i = 0; i<staff.length; i++){
+        var select = document.getElementById(i)
+        valuesToSubmit.push({
+            staff_id: staff[i].id,
+            new_desk_id: $("#staff_"+staff[i].id+" select").val(),
+        });
+    }
+    return valuesToSubmit;    
+}
+
+function submitToDataBase(){
+    var valuesToSubmit = getValuesToSubmit();
+    if(validate()){
+        $.ajax({
+            url: url+"/sendDataToDataBase",
+            method: "POST",
+            data: {
+                values : valuesToSubmit,
+                uuid: uuid,
+            },
+            success: function(){
+            }
+        });
+    }
+}
+function getDesks(){
+    $.ajax({
+        url: url+"/getDesks",
+        method: "GET",
+        success: function(res){
+            for(var currentDesk of res){
+                desks.push(currentDesk);
+            }
+            //console.log(desks)
+            display();
+        }
+    });
+}
+function getStaff(){
+    $.ajax({
+        url: url+"/getStaff",
+        method: "GET",
+        success: function(res){
+            for(var currentStaff of res){
+                staff.push(currentStaff);
+            }
+            //console.log(staff)
+            getDesks();
+        }
+    });
+}
+function connect(){
+    $.ajax({
+        url: url+"/connect",
+        method: "POST",
+        data: {
+            username: username,
+        },
+        success: function(res){
+            uuid = res;
+            console.log("Connected : "+username);
+            console.log("UUID : "+uuid);
+            socket = io(url);
+            socket.on('change', function(data){
+                if(data != uuid){
+                    alert("A change has been made by another user");
+                    staff = [];
+                    desks = [];
+                    $.ajax({
+                        url: url+"/getStaff",
+                        method: "GET",
+                        success: function(res){
+                            console.log(res)
+                            for(var i = 0;i<res.length;i++){
+                                if($("#"+res[i].id).find("[selected]").val() != res[i].desk_id){
+                                    $("#"+res[i].id).find("[selected]").removeAttr("selected")
+                                    $("#"+res[i].id).find("[value='"+res[i].desk_id+"']").attr("selected","selected");
+                                    $("#staff_"+res[i].id).addClass("updated")
+                                }
+                            }
+                        }
+                    })
+                    validate();
+                }
+            });
+        },
+    });
+    getStaff();
+}
+window.onbeforeunload = function(event){
+    $.ajax({
+        url: url+"/disconnect",
+        method: "POST",
+        data: {
+            uuid: uuid,
+        },
+    });
+}
+
+$(function(){
+    $('body').on('change', 'select', function(){
+        validate();
+    });
+});
---
+++ server.js
@@ -0,0 +1,197 @@
+function line(){
+    console.log("------------------------------------------------------------------");
+}
+function getIP(req){
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    ip = ip.split("");
+    ip.splice(0,7);
+    ip = ip.join("");
+    return ip
+}
+const app = require('express')();
+console.log('Express successful')
+const cors = require('cors');
+console.log('Cors successful')
+const server = require('http').createServer(app);
+console.log('HTTP successful')
+const io = require('socket.io')(server);
+console.log('Socket.io successful')
+const bodyParser = require('body-parser');
+console.log('Body-parser successful')
+const uuid = require('uuid/v4');
+console.log('UUID/v4 successful')
+const mysql = require('mysql');
+console.log('MYSQL successful')
+const fs = require('fs');
+console.log('FS successful');
+server.listen(3000);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+line();
+var connections = [];
+var blocked = [];
+var requests = ["XXX"];
+var mysqlConnection = mysql.createConnection({
+    host        : "localhost",
+    user        : "admin",
+    password    : "admin",
+    database    : "desk",
+});
+/*// same
+function(param1, param2) {}
+(param1, param2) => {}
+// same
+function(param1) {}
+param1 => {}
+(param1) => {}
+// same
+function(param1) {
+    return param1 * 5
+}
+(param1) => {
+    return param1 * 5
+}
+param1 => param1 * 5*/
+function validate(results, desks, uuid, req){
+    for(var i = 0; i<results.length;i++){
+        results[i] = parseInt(results[i].id)
+    }
+    for(var i=0;i<desks.length;i++){
+        if(results.indexOf(parseInt(desks[i].new_desk_id)) == -1){
+            console.log("ERROR, did not update")
+            return
+        }
+    }
+    if(blocked.indexOf(getIP(req)) >= 0){
+        return
+    }
+    for(var i = 0;i<desks.length;i++){
+        for(var j = 0 ;j<desks.length;j++){
+            if(i != j && desks[i].new_desk_id == desks[j].new_desk_id){
+                return
+            }
+        }
+    }
+    var promises = [];
+    for(var i = 0; i<desks.length;i++){
+        var myPromise = new Promise((resolve,reject) => {
+            var sql = "UPDATE staff SET desk_id = ? WHERE id = ?";
+            var inserts = [desks[i].new_desk_id, desks[i].staff_id];
+            sql = mysqlConnection.format(sql, inserts);
+            mysqlConnection.query(sql, function(error,qResults,fields){
+                if(error){
+                    reject(error); return;
+                }
+                resolve(qResults);
+            });
+        });
+        promises.push(myPromise);
+    }
+    Promise.all(promises)
+        .then(qResults => {
+            //console.log(qResults)
+            io.emit('change', uuid);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+};
+function getDesks(desks,uuid, req){
+    mysqlConnection.query("SELECT d.id, d.desk_code FROM desks d", function(error,results,fields){
+        if(error){
+            console.log(error)
+            res.status(404);
+            res.send(error);
+            res.end();
+            return;
+        }
+        validate(results, desks, uuid, req);
+    });
+};
+app.post("/sendDataToDataBase", function(req,res){
+    var data = req.body
+    var uuid = data.uuid;
+    data = data.values;
+    getDesks(data,uuid, req);
+    ip = getIP(req);
+    var needPush = true;
+    for(var i=1;i<requests.length;i++){
+        if(requests[i].ip == ip){
+            if(Date.now()-requests[i].last<1000){
+                requests[i].offenses++
+            }
+            requests[i].last = Date.now()
+            needPush = false;
+        }
+    }
+    if(needPush == true){
+        requests.push({
+            ip: ip,
+            last: Date.now(),
+            offenses: 0,
+        });
+    }
+    for(var i=1;i<requests.length;i++){
+        if(requests[i].offenses > 10 && blocked.indexOf(ip) == -1){
+            blocked.push(ip)
+            console.log("IP: "+ip+" has been blocked")
+            line();
+        }
+    }
+    res.end();
+});
+app.post("/connect",function(req, res){
+    tempUUID = uuid();
+    connections.push({
+        username: req.body.username,
+        uuid: tempUUID,
+    });
+    res.send(tempUUID);
+    console.log("New Connection : "+tempUUID)
+    line();
+    res.end();
+});
+app.post("/disconnect",function(req, res){
+    for(var i = 0;i<connections.length;i++){
+        if(connections[i].uuid == req.body.uuid){
+            console.log("User Disconnected : "+connections[i].username)
+            line();
+            var tempDisconnect = connections.splice(i,1);
+            break;
+        }
+    }
+    res.end();
+});
+app.get("/getStaff",function(req, res){
+    mysqlConnection.query("SELECT s.id, s.name, s.desk_id FROM staff s ORDER BY `name`", function(error,results,fields){
+        if(error){
+            console.log(error)
+            res.status(404);
+            res.send(error);
+            res.end();
+            return;
+        }
+        res.send(results);
+        res.end();
+    });
+});
+app.get("/getDesks",function(req, res){
+    mysqlConnection.query("SELECT d.id, d.desk_code FROM desks d", function(error,results,fields){
+        if(error){
+            console.log(error)
+            res.status(404);
+            res.send(error);
+            res.end();
+            return;
+        }
+        res.send(results);
+        res.end();
+    });
+});
