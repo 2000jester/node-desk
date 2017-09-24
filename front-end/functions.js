2ec6e6ad693cbf6dedc2e2cd07a449018898dd7a
Jesse Dux <jesse.dux@4mation.com.au>
2017-09-25 09:19:45 +1000

    added jquery scrollTop

--- front-end/functions.js
+++ front-end/functions.js
@@ -128,11 +128,18 @@ function connect(){
                         url: url+"/getStaff",
                         method: "GET",
                         success: function(res){
+                            var firstInstance = true;
                             for(var i = 0;i<res.length;i++){
                                 if($("#"+res[i].id).find("[selected]").val() != res[i].desk_id){
                                     $("#"+res[i].id).find("[selected]").removeAttr("selected")
                                     $("#"+res[i].id).find("[value='"+res[i].desk_id+"']").attr("selected","selected");
                                     $("#staff_"+res[i].id).addClass("updated")
+                                    if(firstInstance == true){
+                                        $("html, body").animate({
+                                            scrollTop: $("#staff_"+res[i].id).offset().top
+                                        }, 1000);
+                                    }
+                                    firstInstance = false;
                                 }
                             }
                             setTimeout(function(){
