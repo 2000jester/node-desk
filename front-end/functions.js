7b7bdb12965a289e00d7c40f178c51ac80495052
2000jester <2000jester@gmail.com>
2017-09-25 15:09:40 +1000

    generic push

--- front-end/functions.js
+++ front-end/functions.js
@@ -130,7 +130,7 @@ function connect(){
                             var first = true;
                             for(var i = 0;i<res.length;i++){
                                 if($("#"+res[i].id).find("[selected]").val() != res[i].desk_id){
-                                    $("#"+res[i].id).val(res[i].desl_id);
+                                    $("#"+res[i].id).val(res[i].desk_id);
                                     //$("#"+res[i].id).find("[value='"+res[i].desk_id+"']").prop("selected",true);
                                     $("#"+res[i].id).find("[value!='"+res[i].desk_id+"']").prop("selected",false);
                                     $("#staff_"+res[i].id).addClass("updated")
