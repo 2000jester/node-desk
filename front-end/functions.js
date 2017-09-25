b323bd3d5066bd78142f0dc4cd2a688e54966fe9
Jesse Dux <jesse.dux@4mation.com.au>
2017-09-25 10:54:21 +1000

    idk

--- front-end/functions.js
+++ front-end/functions.js
@@ -132,7 +132,9 @@ function connect(){
                             for(var i = 0;i<res.length;i++){
                                 if($("#"+res[i].id).find("[selected]").val() != res[i].desk_id){
                                     $("#"+res[i].id).find("[selected]").removeAttr("selected")
-                                    $("#"+res[i].id).find("[value='"+res[i].desk_id+"']").attr("selected","selected");
+                                    setTimeout(function(){
+                                        $("#"+res[i].id).find("[value='"+res[i].desk_id+"']").attr("selected","selected");
+                                    }, 2);
                                     $("#staff_"+res[i].id).addClass("updated")
                                     if(firstInstance == true){
                                         $("html, body").animate({
