a46b0f56471dfa474fed412da999dab2414ed83b
2000jester <2000jester@gmail.com>
2017-09-25 11:10:03 +1000

    generic push

--- front-end/functions.js
+++ front-end/functions.js
@@ -135,7 +135,7 @@ function connect(){
                             for(var i = 0;i<res.length;i++){
                                 if($("#"+res[i].id).find("[selected]").val() != res[i].desk_id){
                                     $("#"+res[i].id).find("[selected]").removeAttr("selected")
-                                    setTimeout(function(){
+                                    setTimeout(function(i){
                                         console.log(res)
                                         $("#"+res[i].id).find("[value='"+res[i].desk_id+"']").attr("selected","selected");
                                     }, 2);
