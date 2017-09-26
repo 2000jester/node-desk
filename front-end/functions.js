ef080b1b4bbcaf80924ad8422314ec16634744f1
2000jester <2000jester@gmail.com>
2017-09-26 14:13:33 +1000

    generic push

--- front-end/functions.js
+++ front-end/functions.js
@@ -58,6 +58,7 @@ function updateList(target){
     if(changed.length > 0){
         if(_.findIndex(changed, {id: target.id}) > -1){
             if(obj.new_desk_id != obj.old_desk_id){
+                obj = prepareOBJ(target);
                 drawList();
                 return
             } else if(obj.new_desk_id == obj.old_desk_id){
