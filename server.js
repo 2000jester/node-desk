771a10543fefee0c4650c68c5a12e94beadbc934
2000jester <2000jester@gmail.com>
2017-09-26 10:14:13 +1000

    generic push

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ fs.readFile("blocked.txt", "utf8",function(error,data){
     }
     blocked = data.split(",")
     console.log(blocked.length)
-    if(blocked.length>0){
+    if(blocked[i].length>0){
         console.log("Blocked IPs")
         for(var i = 0;i<blocked.length;i++){
             console.log(blocked[i])
